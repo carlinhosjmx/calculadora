42d4ce14210a18425145c5f7a4f8ec2e61e28007
Carlos xavier <carlinhosjmx@gmail.com>
2020-09-07 19:51:03 -0300

    subindo arquivos

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,147 @@
+
+
+class CalcController{
+
+    constructor()
+    {
+        this._operation = [];
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+        this.initButtonsEvent();
+    }
+
+    initialize()
+    {
+        this.setDisplayDateTime();
+            
+        setInterval(()=>{
+            this.setDisplayDateTime();
+        }, 1000);
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    addEventListenerAll(element,events,fn)
+    {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    initButtonsEvent()
+    {
+        let buttons =  document.querySelectorAll("#buttons > g, #parts > g");
+        
+        buttons.forEach((btn, index )=>{
+            this.addEventListenerAll(btn,'click drag', e =>{
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+               
+                this.execBtn(textBtn);
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+                  btn.style.cursor = "pointer";
+            });
+        });
+    }
+
+    clearAll()
+    {
+        this._operation = [];
+    }
+
+    clearEntry()
+    {
+        this._operation.pop();
+    }
+
+    addOperation(value)
+    {   console.log(value);
+        if(isNumber(value)){
+            let valor = value 
+        }
+        this._operation.push(value);
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+        console.log("value",value);
+         switch(value){
+            case 'ac':
+                this.clearAll();
+                 break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                break;
+            case 'subtracao':
+                break;
+            case 'divisao':
+                break;
+            case 'mutiplicacao':
+                break;
+            case 'igual':
+                break;
+            case 'porcento':
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+         }
+    }
+
+     get displayTime(){
+        return this._timeEl;
+    }
+
+    set displayTime(value){
+        this._timeEl.innerHTML = value ;
+    }
+
+    get displayDate(){
+        return this._dateEl;
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value ;
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl;
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date;   
+     }
+
+    set currentDate(data){
+        this._currentDate = data;
+    }
+}
